a9f59981706a84b687b3d9617acd8631c37128b5
Unknown <yannick.jacques940@gmail.com>
2018-11-27 17:09:30 -0500

    nothing

--- controlleur/AddImageInDatabase.php
+++ controlleur/AddImageInDatabase.php
@@ -4,7 +4,7 @@
             if(is_uploaded_file($_FILES['fichier_source']['tmp_name'])){
                 echo '<p>uploaded</p>';
                 if(filesize($_FILES['fichier_source']['tmp_name'])<2000000){
-                    echo'<p> taille correct</p>';
+                    require("view/view_add_image.php");
                 }
             }
         }
@@ -22,13 +22,20 @@ function traite_initial(){
     $extension_autorisee = array('jpg', 'jpeg', 'gif', 'png','JPG', 'JPEG', 'GIF', 'PNG');
 		if(in_array($extension_upload,$extension_autorisee))
 		{
-			move_uploaded_file($_FILES['fichier_source']['tmp_name'], "Images/".$nom_fichier.'.'. $extension_upload);
+			$path ="Images/".$nom_fichier.'.'. $extension_upload;
+			move_uploaded_file($_FILES['fichier_source']['tmp_name'], $path);
+			SavePathInDataBase($path);
 			return true;
 		}
 	}
 
 	function GetNewFileName()
 	{
-		return date('Ymd-H_i_s');
+		return date('YmdHis');
+	}
+
+	function SavePathInDataBase()
+	{
+	
 	}
 ?>
