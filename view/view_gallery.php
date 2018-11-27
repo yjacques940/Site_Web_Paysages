106a94350f2698d62bd1e6b8790100922c949d3f
Unknown <yannick.jacques940@gmail.com>
2018-11-27 17:42:18 -0500

    Faire afficher toutes les photos dans gallery

--- view/view_gallery.php
+++ view/view_gallery.php
@@ -13,7 +13,30 @@ ob_start();
 </head>
 
 	<body>
-		<p> This is a Paragraph </p>
+	<div>
+	<?php
+                $directory = "Images";
+				$images = glob($directory . "/*.{jpg,jpeg,gif,png,JPG,JPEG,GIF,PNG}",GLOB_BRACE);
+				$count =0;
+				foreach($images as $image)
+				{
+				$count++;
+				?>
+		<div class="photo-box pure-u-1 pure-u-md-1-2 pure-u-lg-1-3">
+            <a href="http://www.nilssonlee.se/">
+			<?php
+				  echo '<img src="' .$image . '"/>';
+			?>
+            </a>
+            <aside class="photo-box-caption">
+                <span>
+                    by <a href="http://www.nilssonlee.se/"><?php echo $count ?></a>
+                </span>
+            </aside>
+        </div>
+		<?php } ?>
+
+	</div>
 	</body>
 
 </html>
